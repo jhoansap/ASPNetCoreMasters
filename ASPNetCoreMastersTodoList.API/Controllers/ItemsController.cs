f6d1c20ef22b77d66c3c8aa4c98fe31c1237c428
Jhoan Sapiera <jhoanp@magenic.com>
2021-09-19 19:32:24 +0800

    Session2 Homework

---
+++ ASPNetCoreMastersTodoList.API/Controllers/ItemsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreMastersTodoList.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly ItemService _itemService;
+
+        public ItemsController()
+        {
+
+            this._itemService = new ItemService();
+
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get(int Id)
+        {
+
+            return _itemService.GetAll(Id);
+
+        }
+
+    }
+}
---
+++ Services/ItemService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Services
+{
+    public class ItemService
+    {
+        public IEnumerable<string> GetAll(int userId)
+        {
+            IEnumerable<string> empty = Enumerable.Empty<string>();
+            return empty;
+        }
+    }
+}
