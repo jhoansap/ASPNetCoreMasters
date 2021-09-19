2f96ae285cc607394a70a27cadb9501e5986e89e
Jhoan Sapiera <jhoanp@magenic.com>
2021-09-20 00:05:52 +0800

    Session3 Homework

---
+++ DomainModels/Item.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DomainModels
+{
+    public class Item
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ Services/DTO/ItemDTO.cs
@@ -0,0 +1,12 @@
+﻿using DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.DTO
+{
+    public class ItemDTO:Item
+    {
+
+    }
+}
