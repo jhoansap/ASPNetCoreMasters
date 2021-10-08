3805b00e8dcb4ae08e2841e30ea7021000ed6198
Jhoan Sapiera <jhoanp@magenic.com>
2021-10-08 22:58:35 +0800

    Add PerformanceResourceFilter write the request elapsed time (ms) in console

---
+++ ASPNetCoreMastersTodoList.API/Filters/PerformanceResourceFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreMastersTodoList.API.Filters
+{
+    public class PerformanceResourceFilter : IResourceFilter
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            _stopwatch.Start();
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            _stopwatch.Stop();
+            Console.WriteLine("Request Elapsed Time: " + this._stopwatch.ElapsedMilliseconds + " ms");
+        }
+    }
+}
